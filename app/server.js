289c2922ec1e2c7321501129e6fe8904cd556f83
Michael Gamlem III <mgamlem3@gmail.com>
2020-03-20 19:57:19 -0700

    add volume

--- app/server.js
+++ app/server.js
@@ -6,9 +6,9 @@ const port = 80;
 
 app.use(express.static('public'))
 
-app.get("/", (req, res) => res.sendFile("/index.html"));
+app.get("/", (req, res) => res.sendFile("index.html"));
 app.get("/.well-known*", function (req, res) {
-	res.sendFile(fs.readFileSync(req.url));
+	res.sendFile(fs.readFileSync('/etc/letsencrypt' + req.url));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
