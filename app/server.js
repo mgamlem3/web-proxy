a6be50bad7a0266b7a75cd3bfd6bd32c5b92e4df
Michael Gamlem III <mgamlem3@gmail.com>
2020-03-22 18:33:50 -0700

    update server.js

--- app/server.js
+++ app/server.js
@@ -1,14 +1,22 @@
-const express = require('express');
+/** @format */
+
+const express = require("express");
 const fs = require("fs");
+const serveIndex = require("serve-index");
 
 const app = express();
 const port = 80;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get("/", (req, res) => res.sendFile("index.html"));
-app.get("/.well-known*", function (req, res) {
-	res.sendFile(fs.readFileSync('/etc/letsencrypt' + req.url));
+app.get("/public", function(req, res) {
+	res.status(200).send("okay");
 });
-
+app.get("/.well-known*", function(req, res) {
+	res.sendFile(fs.readFileSync("/etc/letsencrypt" + req.url));
+});
+app.get("/", function(req, res) {
+	console.log(req.url);
+	res.sendFile("/index.html");
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
