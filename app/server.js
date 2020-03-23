1754d7b53cd1f314d6ab209a3f27961bbcb2f227
Michael Gamlem III <mgamlem3@gmail.com>
2020-03-23 00:00:17 -0700

    add https

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const fs = require("fs");
+const https = require("https");
 const serveIndex = require("serve-index");
 
 const app = express();
@@ -12,20 +13,26 @@ app.use(express.static("public"));
 app.get("/public", function(req, res) {
 	res.status(200).send("okay");
 });
-app.get("/.well-known*", function (req, res) {
+app.get("/.well-known*", function(req, res) {
 	const string = "/etc/letsencrypt" + req.url;
 	console.log(string);
 	const file = fs.readFileSync(string.toString());
 	console.log(file);
 	res.sendFile(string);
 });
-app.use(
-	"/ls",
-	express.static("/etc/letsencrypt"),
-	serveIndex("/etc/letsencrypt", { icons: true }),
-);
-
 app.get("/", function(req, res) {
 	res.status(200).send("index");
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+https
+	.createServer(
+		{
+			key: fs.readFileSync("/etc/letsencrypt/mgamlem3/privkey.pen"),
+			cert: fs.readFileSync("/etc/letsencrypt/mgamlem3/fullchain.pem"),
+		},
+		app,
+	)
+	.listen(443, function() {
+		console.log("Example app listening on port 443!");
+	});
