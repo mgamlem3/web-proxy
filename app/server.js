f173fb70330c57eee0aeae4d85ff00f121bdb29b
Michael Gamlem III <mgamlem3@gmail.com>
2020-03-23 22:49:51 -0700

    add path

--- app/server.js
+++ app/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const fs = require("fs");
 const https = require("https");
 const serveIndex = require("serve-index");
+const path = require("path");
 
 const app = express();
 const port = 80;
