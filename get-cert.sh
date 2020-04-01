d884ebe7711a36eb7028220786261371f86e6176
Michael Gamlem III <mgamlem3@gmail.com>
2020-03-31 23:17:00 -0700

    update cert info

--- get-cert.sh
+++ get-cert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-certbot certonly --webroot -w /home/michael/proxy-data -d mgamlem3.com -d git.mgamlem3.com --dry-run
+certbot certonly --webroot -w /home/michael/proxy-data -d mgamlem3.com -d git.mgamlem3.com --dry-run --debug
 cp -a -L -u /etc/letsencrypt/live/mgamlem3.com/. /home/michael/proxy-data/current-certs
