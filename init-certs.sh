d8e7aaba1b71a6a2a4bfe602313433191dfb7c46
Michael Gamlem III <mgamlem3@gmail.com>
2020-04-03 00:21:36 -0700

    add mail route

--- init-certs.sh
+++ init-certs.sh
@@ -9,7 +9,7 @@ domains=(mgamlem3.com, www.mgamlem3.com, git.mgamlem3.com, mail.mgamlem3.com)
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
