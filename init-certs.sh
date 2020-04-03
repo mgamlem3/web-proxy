f210e52f69d446ab4bd5fb69a4c896ddeac0a9e3
Michael Gamlem III <mgamlem3@gmail.com>
2020-04-03 00:28:02 -0700

    update init-certs

--- init-certs.sh
+++ init-certs.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(mgamlem3.com, www.mgamlem3.com, git.mgamlem3.com, mail.mgamlem3.com)
+domains=(mgamlem3.com www.mgamlem3.com git.mgamlem3.com mail.mgamlem3.com)
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
@@ -27,34 +27,37 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   echo
 fi
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:1024 -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
-echo
-
+for domain in ${domains[@]}; do
+	echo "### Creating dummy certificate for $domain ..."
+	path="/etc/letsencrypt/live/$domain"
+	mkdir -p "$data_path/conf/live/$domain"
+	docker-compose run --rm --entrypoint "\
+	openssl req -x509 -nodes -newkey rsa:1024 -days 1\
+		-keyout '$path/privkey.pem' \
+		-out '$path/fullchain.pem' \
+		-subj '/CN=localhost'" certbot
+	echo
+done
 
 echo "### Starting nginx ..."
 docker-compose up --force-recreate -d nginx
 echo
 
-echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-echo
-
+for domain in ${domains[@]} do 
+	echo "### Deleting dummy certificate for $domain ..."
+	docker-compose run --rm --entrypoint "\
+	rm -Rf /etc/letsencrypt/live/$domain && \
+	rm -Rf /etc/letsencrypt/archive/$domain && \
+	rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
+	echo
+done
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
 #Join $domains to -d args
 domain_args=""
 for domain in "${domains[@]}"; do
   domain_args="$domain_args -d $domain"
+  echo "d args: $domain_args"
 done
 
 # Select appropriate email arg
