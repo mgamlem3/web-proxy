d64fe4dcdb55bc81713a9a6a6162e9a219db527b
Michael Gamlem III <mgamlem3@gmail.com>
2020-04-03 00:19:11 -0700

    add mail route

--- init-certs.sh
+++ init-certs.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(mgamlem3.com www.mgamlem3.com git.mgamlem3.com mail.mgamlem3.com)
+domains=(mgamlem3.com, www.mgamlem3.com, git.mgamlem3.com, mail.mgamlem3.com)
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
